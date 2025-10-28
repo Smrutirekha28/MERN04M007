0427426b2db57ae2d749d9415f1a782e126675c7
Smrutirekha28 <smruti182005@gmail.com>
2025-10-28 19:35:57 +0530

    operator

---
+++ oct/week5/day2/README.md
@@ -0,0 +1,12 @@
+
+Operator
+=======================
+special types of symbol which perform opertaion on data
+->arithmatic(+,-,*,/,%,**,++,--)
+->assignment(=,+=,-=,*=,%=,**=)
+->comparison(==,===,!=,!==,>,>=,<,<=)(result boolean data type(true,false))
+->logical (&&,||,!)
+
+
+
++ has 3 work(concatenation,addition,type cating)
---
+++ oct/week5/day2/script.js
@@ -0,0 +1,89 @@
+//......................OPERATOR...........................
+console.log("today we are going to learn operator");
+
+console.log(2+2);
+let a =2;
+let b=3;
+console.log(a+b);
+
+console.log((a+2)+b);
+
+
+
+let c;
+let d;
+console.log(c+d);//NaN
+
+console.log(typeof (c+d));
+
+let g =2;
+let e=3;
+console.log(g-e);
+console.log(g*e);
+console.log(g/e);
+
+// console.log(b%a);
+// console.log(a**b);
+// console.log(a++);//post increment (1st use then increment / 1st print then update)
+// console.log(++a);// pre increment (1st update then print / 1st increment then print)
+
+console.log(a--);
+console.log(--a);
+
+// let s=w;
+// console.log(s);// error: w is not defined
+
+
+//  let q=2;
+// //  q=q+2;
+//  q+=2
+//  console.log(q);
+ 
+//  let q=2;
+//  q-=2
+//  console.log(q);
+
+
+//  let q=2;
+//  q%=2
+//  console.log(q);
+
+
+//  let q=2;
+//  q**=2
+//  console.log(q);
+
+
+ let q=2;
+ q*=2
+ console.log(q);
+
+
+
+
+
+ //comparison operator
+ console.log(a<=b);
+
+
+ let num1=2;
+ let num2="1";
+//  console.log(num1==num2);
+//  console.log(num1 === num2);// it checks 1st  type thn value
+ 
+ console.log(num1!=num2);
+ console.log(num1 !== num2);
+ 
+ 
+ 
+console.log((3>2)&&(4>5));
+console.log((3>2)||(4>5));
+console.log(!true);
+console.log(!false);
+
+console.log((num2++ >4) && (num1===num2)||(num1-- != num2));
+
+
+let n="1";
+console.log(n-1);
+console.log(1+3-1+    +n+4+"5");
